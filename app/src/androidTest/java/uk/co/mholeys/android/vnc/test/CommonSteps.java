73b4ba53147171df4b19368a4a9d1fbe3b70635b
Matthew <matthew.holey@googlemail.com>
2018-01-13 15:32:01 +0000

    Added screenshot for failed tests

--- app/src/androidTest/java/uk/co/mholeys/android/vnc/test/CommonSteps.java
+++ app/src/androidTest/java/uk/co/mholeys/android/vnc/test/CommonSteps.java
@@ -1,13 +1,26 @@
 package uk.co.mholeys.android.vnc.test;
 
+import android.os.Environment;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.ViewAssertion;
+import android.util.Log;
 import android.widget.Button;
 
+import org.junit.Rule;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+import java.io.File;
+
 import cucumber.api.CucumberOptions;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
 import cucumber.api.java.en.When;
 import uk.co.mholeys.android.vnc.*;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -18,6 +31,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.instanceOf;
+
+import android.support.test.uiautomator.UiDevice;
 
 /**
  * Created by Matthew on 11/01/2018.
@@ -51,6 +66,24 @@ public class CommonSteps {
     @When("^I long press (\\S+)$")
     public void I_long_press_x(String item) {
         onView(withText(item)).perform(longClick());
+    }
+
+    @After
+    public void commonAfter(Scenario scenario) {
+        if (scenario.isFailed()) {
+
+            // Save to external storage (usually /sdcard/screenshots)
+            File path = new File(Environment.getExternalStorageDirectory().getAbsolutePath()
+                    + "/screenshots/" + getTargetContext().getPackageName());
+            if (!path.exists()) {
+                path.mkdirs();
+            }
+
+            // Take advantage of UiAutomator screenshot method
+            UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+            String filename = scenario.getId() + "." + scenario.getName() + ".png";
+            device.takeScreenshot(new File(path, filename));
+        }
     }
 
 }
